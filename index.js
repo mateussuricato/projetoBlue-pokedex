d829f1b7603d1f0b1f631c433aa2475dc9fdce8e
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-04-20 13:32:14 -0300

    pokedex

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ const __dirname = path.resolve(path.dirname(''))
 
 const app = express()
 
+const pokedex = [
+    {
+        id: 1,
+        nome: 'Bulbasaur',
+        descricao: 'Há uma semente de planta nas costas desde o dia em que este Pokémon nasce. A semente cresce lentamente.',
+        tipo: 'Glass',
+        imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'
+    },
+    {
+        id: 2,
+        nome: 'Charmander',
+        descricao: "A chama que queima na ponta da sua cauda é um indicador de suas emoções. A chama oscila quando Charmander está contente. Se o Pokémon fica com raiva, a chama queima violentamente.",
+        tipo: 'Fire',
+        imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png'
+    },
+    {
+        id: 3,
+        nome: 'Squirtle',
+        descricao: "A carapaça de Squirtle não serve só para sua proteção. As ranhuras em sua superfície e a sua forma arredondada ajudam a minimizar a resistência na água permitindo que nade em alta velocidade.",
+        tipo: 'Water',
+        imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png'
+    }
+]
+
 app.set("view engine", "ejs") // reconhecendo o ejs
 app.use(express.static(path.join(__dirname, 'public'))) //reconhecendo a pasta public
 
@@ -15,7 +39,7 @@ app.listen(port, () => {
 })
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index.ejs', {pokedex})
 })
 
 app.get('/detalhes', (req, res) => {
